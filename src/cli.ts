3f456bce03a607be244144c4067b61e44f2ea768
cinfinite <cinfinite@192.168.1.10>
2025-06-23 02:50:22 +0530

    cli version change

--- src/cli.ts
+++ src/cli.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name('component-dependency-collapser')
   .description('Collapse and analyze dependencies of frontend components')
-  .version('1.0.0');
+  .version('1.0.2');
 
 program
   .argument('<Dir>', 'directory')
